44607ccac632dc9fcf1a3a9850affd39b768f4bf
onon <gantumuronon539@gmail.com>
2021-04-12 12:35:42 +0800

    peer connection

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ io.on('connection', socket=>{
     socket.on('join-room', (roomId, userId)=>{
         socket.join(roomId)
         socket.broadcast.to(roomId).emit('user-connected', userId)
+        socket.on('disconnect', ()=>{
+            socket.broadcast.to(roomId).emit('user-disconnected', userId)
+        })
     })
+
+    
 })
 server.listen(port)
